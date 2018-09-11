2b85e52aa14537305241c21f285b17a5e256f36f
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-09-11 11:53:47 +0800

    added ITimeInquiryRepository that will be implemented in infrastructure project

---
+++ Clockwork.Domain/Repositories/ITimeInquiryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Clockwork.Domain.Entities;
+using Clockwork.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clockwork.Domain.Repositories
+{
+    public interface ITimeInquiryRepository
+    {
+        PagedResult<TimeInquiryEntity> GetAll(int pageNumber, int pageSize);
+        void Create(TimeInquiryEntity entity);
+    }
+}
