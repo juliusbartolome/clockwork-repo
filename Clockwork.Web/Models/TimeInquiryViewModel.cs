fafe1e6a46b0d6618d9604db572712a6bcfd9488
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-09-12 05:33:00 +0800

    added a viewmodel for timeinquiry controller

---
+++ Clockwork.Web/Models/TimeInquiryViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace Clockwork.Web.Models
+{
+    public class TimeInquiryViewModel
+    {
+        public string SelectedTimeZoneInfoStandardName { get; set; }
+    }
+}
