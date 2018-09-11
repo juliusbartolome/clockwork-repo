ee238c515105b80054f5cca930baa0bced0c5c06
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-09-11 12:13:12 +0800

    added ef core repostiory implementations

---
+++ Clockwork.Infrastructure.EFCore/Repositories/EfCoreBaseRepository.cs
@@ -0,0 +1,48 @@
+﻿using Clockwork.Infrastructure.EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clockwork.Infrastructure.EFCore.Repositories
+{
+    public class EfCoreBaseRepository<TDbContext, TEntity> : IDisposable
+        where TDbContext : DbContext
+        where TEntity : class
+    {
+        private bool _disposed = false;
+
+        protected readonly TDbContext Context;
+        protected readonly DbSet<TEntity> EntitySet;
+
+        public EfCoreBaseRepository()
+        {
+            Context = (TDbContext)Activator.CreateInstance(typeof(TDbContext));
+            EntitySet = Context.Set<TEntity>();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                Context.Dispose();
+            }
+
+            _disposed = true;
+        }
+
+        ~EfCoreBaseRepository()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ Clockwork.Infrastructure.EFCore/Repositories/TimeInquiryRepository.cs
@@ -0,0 +1,33 @@
+﻿using Clockwork.Domain.Entities;
+using Clockwork.Domain.Repositories;
+using Clockwork.Domain.Shared;
+using Clockwork.Infrastructure.EFCore.Models;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clockwork.Infrastructure.EFCore.Repositories
+{
+    public class TimeInquiryRepository : EfCoreBaseRepository<ClockworkContext, TimeInquiry>, ITimeInquiryRepository
+    {
+        public void Create(TimeInquiryEntity entity)
+        {
+            var newTimeInquiryEntry = entity.Adapt<TimeInquiryEntity, TimeInquiry>();
+
+            EntitySet.Add(newTimeInquiryEntry);
+            Context.SaveChanges();
+
+            newTimeInquiryEntry.Adapt(entity);
+        }
+
+        public PagedResult<TimeInquiryEntity> GetAll(int pageNumber, int pageSize)
+        {
+            var pageItems = EntitySet.Skip(pageNumber - 1).Take(pageSize).ProjectToType<TimeInquiryEntity>().ToList();
+            var totalItemsCount = EntitySet.Count();
+
+            return new PagedResult<TimeInquiryEntity>(pageItems, pageNumber, pageSize, totalItemsCount);
+        }
+    }
+}
