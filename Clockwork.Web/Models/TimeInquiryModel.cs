687c2c4f5a279f74563a75153fc44e0d6b891700
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-09-12 05:32:40 +0800

    added computed property server time in time inquiry model

--- Clockwork.Web/Models/TimeInquiryModel.cs
+++ Clockwork.Web/Models/TimeInquiryModel.cs
@@ -8,6 +8,14 @@ namespace Clockwork.Web.Models
         public DateTime UtcDateTime { get; set; }
         public string TimeZoneStandardName { get; set; }
         public string IpAddress { get; set; }
-        public DateTime ServerTime { get; set; }
+
+        public DateTime ServerTime
+        {
+            get
+            {
+                var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(TimeZoneStandardName) ?? TimeZoneInfo.Local;
+                return TimeZoneInfo.ConvertTimeFromUtc(UtcDateTime, timeZoneInfo);
+            }
+        }
     }
 }
