b92b366e698d3afd5c5f6bd04bb945c3cc9addb7
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-09-11 12:12:41 +0800

    added EF Core setup

---
+++ Clockwork.Infrastructure.EFCore/Models/ClockworkContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clockwork.Infrastructure.EFCore.Models
+{
+    public class ClockworkContextFactory : IDesignTimeDbContextFactory<ClockworkContext>
+    {
+        public ClockworkContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ClockworkContext>();
+            optionsBuilder.UseSqlite("Data Source=../Clockwork.API/clockwork.db");
+
+            return new ClockworkContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ Clockwork.Infrastructure.EFCore/Models/TimeInquiry.cs
@@ -0,0 +1,16 @@
+﻿using Clockwork.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Clockwork.Infrastructure.EFCore.Models
+{
+    public class TimeInquiry
+    {
+        public int Id { get; set; }
+        public DateTime UtcDateTime { get; set; }
+        public string TimeZoneStandardName { get; set; }
+        public string IpAddress { get; set; }
+    }
+}
