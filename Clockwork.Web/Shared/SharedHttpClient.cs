cac977199b477caf9e0544009018b6dddbc238ce
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-09-12 03:42:10 +0800

    added SharedHttpClient that is used by TimeInquieryService
    src: https://aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/

---
+++ Clockwork.Web/Shared/SharedHttpClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace Clockwork.Web.Shared
+{
+    public static class SharedHttpClient
+    {
+        private static HttpClient _httpClient;
+
+        public static HttpClient Instance
+        {
+            get
+            {
+                if (_httpClient == null)
+                    _httpClient = new HttpClient();
+
+                return _httpClient;
+            }
+        }
+    }
+}
