e1aa4a6965e9dad5d7723761760015523105466e
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-09-11 12:42:54 +0800

    removed ef core in api project.

---
+++ Clockwork.API/Controllers/TimeInquiriesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Clockwork.API.Models;
+using Clockwork.Domain.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Clockwork.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class TimeInquiriesController : Controller
+    {
+        private readonly ITimeInquiryService _timeInquiryService;
+        private readonly ILogger<TimeInquiriesController> _logger;
+
+        public TimeInquiriesController(ITimeInquiryService timeInquiryService, ILogger<TimeInquiriesController> logger)
+        {
+            _timeInquiryService = timeInquiryService;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post(TimeInquiryPostDto timeInquiryPostDto)
+        {
+            var ipAddress = HttpContext.Connection.RemoteIpAddress.ToString();
+            var timeZoneStandardName = timeInquiryPostDto.TimeZoneStandardName;
+            if (string.IsNullOrEmpty(timeZoneStandardName))
+                timeZoneStandardName = TimeZoneInfo.Local.StandardName;
+
+            try
+            {
+                var createdEntity = _timeInquiryService.RecordTimeInquiry(ipAddress, timeZoneStandardName);
+                _logger.LogInformation($"Successfully created a new time inquiry {nameof(createdEntity.Id)}.");
+                return Ok(createdEntity);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"An exception occured in {nameof(TimeInquiriesController)}.{nameof(Post)}.");
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ Clockwork.API/Models/TimeInquiryPostDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clockwork.API.Models
+{
+    public class TimeInquiryPostDto
+    {
+        public string TimeZoneStandardName { get; set; }
+    }
+}
