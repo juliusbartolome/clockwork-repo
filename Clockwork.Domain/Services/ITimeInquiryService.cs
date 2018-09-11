1497e23216af0c433494dadd256bd7031962df71
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2018-09-11 11:54:09 +0800

    added ITimeInquiryService that will be implemented in application project

---
+++ Clockwork.Domain/Services/ITimeInquiryService.cs
@@ -0,0 +1,14 @@
+﻿using Clockwork.Domain.Entities;
+using Clockwork.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clockwork.Domain.Services
+{
+    public interface ITimeInquiryService
+    {
+        TimeInquiryEntity RecordTimeInquiry(string ipAddress);
+        PagedResult<TimeInquiryEntity> GetAll(int pageNumber, int pageSize);
+    }
+}
